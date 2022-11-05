c1ed45c7529e1ea873e3ad09f2ebd8f121c7fbc0
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-11-06 01:15:47 +0600

    Added new solution.

--- Part 1/Problem-39.cpp
+++ Part 1/Problem-39.cpp
@@ -1,7 +1,8 @@
 /*
-  You given N (1 <= N <= 100) number - total amount of elements in the sequence.
-  Then you are given a sequence itself.
-  Your task is to find index of the minimum element in array, assuming that all sequences are started from 0.
+  Position of "-1".
+
+  In the given NxM matrix find the LAST position of the minus one (-1).
+  If it will be no "-1" value in the given matrix just output "-1 -1".
 */
 
 #include <iostream>
@@ -10,24 +11,35 @@ using namespace std;
 
 int main () 
 {
-	int size, i, min = 10000, index;
+	int sizeN, sizeM, i = 0, n = 0, no = 0, a = 1, b = 1;
 
-	cin >> size;
-	int x[size];
+	cin >> sizeN >> sizeM;
 
-	if (size >= 1 && size <= 1000) {
+	if (sizeN >= 1 && sizeM <= 100) {
+		int x[sizeN][sizeM], y[sizeN];
 
-		for (i = 0; i <= (size - 1); i++)
-			cin >> x[i];
+		for (i = 0; i < sizeN; i++)
+			for (n = 0; n < sizeM; n++)
+				cin >> x[i][n];
 
-		for (i = (size - 1); i >= 0; i--) {
-			if (x[i] <= min) {
-				index = i;
-				min = x[i];
+		for (i = 0; i < sizeN; i++) {
+			for (n = 0; n < sizeM; n++) {
+				if (x[i][n] == -1) {
+					a = i + 1;
+					b = n + 1;
+				}
+			}
+			if (x[i][n] == -1) {
+				a = i + 1;
+				b = n + 1;
 			}
 		}
 
-		cout << index;
+		if (x[a - 1][b - 1] != -1) no = 1;
+
+		if (no == 0) cout << a << " " << b;
+		else cout << "-1 -1";
 	}
+
 	return 0;
 }
