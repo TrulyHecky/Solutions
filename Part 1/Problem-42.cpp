ee642e555fb1e48d37070198746e0993fe490371
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-11-06 01:19:17 +0600

    Added new solution.

--- Part 1/Problem-42.cpp
+++ Part 1/Problem-42.cpp
@@ -1,41 +1,34 @@
 /*
-  You given N and one line of length N (1 <= N <= 1000) then consist of letters in interval A..Z.
-  Your task is to find out how often each letter from the alphabet is found in the line.
+  Sum under main diagonal.
+
+  Calculate the total sum of element that are located under main diagonal in a given NxN square table.
 */
 
 #include <iostream>
 
 using namespace std;
 
-int main () 
+int main ()
 {
-	int size, i, n = 0, times[26] = {0};
-	char y[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int size, i = 0, n = 0, m = 1, res = 0;
 
 	cin >> size;
-	char x[size];
 
-	if (size >= 1 && size <= 1000) {
+	if (size >= 1 && size <= 100) {
+		int x[size][size];
 
-		for (i = 0; i <= (size - 1); i++)
-			cin >> x[i];
+		for (i = 0; i < size; i++)
+			for (n = 0; n < size; n++)
+				cin >> x[i][n];
 
-		while (n <= 25) {
-			for (i = (size - 1); i >= 0; i--)
-				if (x[i] == y[n]) times[n]++;
-			n++;
+		for (i = 1; i < size; i++) {
+			for (n = 0; n < m; n++)
+				res += x[i][n];
+			m++;
 		}
 
-		n = 0;
-
-		for (i = 0; i <= 25; i++)
-			if (times[i] > (0)) n++;
-
-		cout << n << endl;
-
-		for (i = 0; i <= 25; i++)
-			if (times[i] != (0)) cout << y[i] << " " << times[i] << endl;
+		cout << res;
 	}
-	
+
 	return 0;
 }
