95564518cdce1df6600e429fb1f5783d326c1394
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-11-15 11:33:46 +0600

    Added new algorithm.

---
+++ Algorithms/Multiply a given Integer with 3.5.cpp
@@ -0,0 +1,17 @@
+// Program to multiply a number with 3.5
+
+#include <iostream>
+using namespace std;
+ 
+int multiplyWith3Point5 (int x)
+{
+    return (x << 1) + x + (x >> 1);
+}
+
+int main ()
+{
+    int x = 4;
+    cout << " "<< multiplyWith3Point5(x);
+    getchar();
+    return 0;
+}
